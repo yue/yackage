977826c5985a3c7c109b572bb54a93e4742d14c7
Cheng Zhao <zcbenz@gmail.com>
2018-03-09 22:34:53 +0900

    Add cli

---
+++ bin/yackage.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const os = require('os')
+const program = require('commander')
+
+const {packageApp} = require('..')
+
+const opts = {
+  yodeVersion: 'v0.2.0',
+  yodePlatform: process.platform,
+  yodeArch: process.arch,
+  cacheDir: os.tmpdir(),
+  options: {},
+}
+
+function action(outputPath, appDir) {
+  Object.assign(opts, program)
+  packageApp(outputPath, appDir, opts.options,
+             opts.yodeVersion, opts.yodePlatform, opts.yodeArch, opts.cacheDir)
+}
+
+program.version('v' + require('../package.json').version)
+       .description('Package Node.js project with Yode')
+       .option('--yode-version',  'Yode version')
+       .option('--yode-platform', 'Yode platform')
+       .option('--yode-arch',     'Yode arch')
+       .option('--cacheDir', 'Directory to store downloaded binaries')
+       .arguments('<outputPath> <appDir>')
+       .action(action)
+       .parse(process.argv)
+
+if (process.argv.length == 2)
+  program.outputHelp()
